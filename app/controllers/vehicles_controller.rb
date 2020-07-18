19aa47be48e6c6db3d391a9b3ef98be33c59157d
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-07-18 15:33:24 +0530

    added vehicles scrapper

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -61,6 +61,18 @@ class VehiclesController < ApplicationController
     end
   end
 
+  def scrape
+    url = 'https://www.cars.com/shopping/sedan/'
+    response = VehiclesSpider.process(url)
+    if response[:status] == :completed && response[:error].nil?
+      flash.now[:notice] = "Successfully scraped url"
+    else
+      flash.now[:alert] = response[:error]
+    end
+  rescue StandardError => e
+    flash.now[:alert] = "Error: #{e}"
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_vehicle
